27f623aef5adea4b763cb9de28a3bc409597183b
adipatixyz <121762056+adipatixyz@users.noreply.github.com>
2024-05-24 10:28:42 +0700

    Up By Gemilang

--- adipati.sh
+++ adipati.sh
@@ -9,6 +9,7 @@
 # Gemilangkinasih࿐
 
 cd /usr/local/sbin
+# Instalasi Create Account
 wget -q -O /usr/local/sbin/trialbot "https://raw.githubusercontent.com/adipatixyz/wabot/main/wabot/trialbot"
 chmod +x /usr/local/sbin/trialbot
 wget -q -O /usr/local/sbin/addsshbot "https://raw.githubusercontent.com/adipatixyz/wabot/main/wabot/addsshbot"
@@ -17,14 +18,19 @@ wget -q -O /usr/local/sbin/trialwsbot "https://raw.githubusercontent.com/adipati
 chmod +x /usr/local/sbin/trialwsbot
 wget -q -O /usr/local/sbin/addwsbot "https://raw.githubusercontent.com/adipatixyz/wabot/main/wabot/addwsbot"
 chmod +x /usr/local/sbin/addwsbot
-
+# Instalasi Backup
+wget -q -O /usr/local/sbin/backupbot "https://raw.githubusercontent.com/adipatixyz/wabot/main/wabot/backupbot"
+chmod +x /usr/local/sbin/backupbot
+# Enc Wabot
+epro *bot
+rm -r *~
+# Instalasi SSHD_Config Port Login 6401
 cd /etc/ssh
 wget -q -O /etc/ssh/sshd_config "https://raw.githubusercontent.com/adipatixyz/wabot/main/wabot/sshd_config"
 chmod +x /etc/ssh/sshd_config
 systemctl restart sshd
 cd
 rm -r adipati.sh
-
 echo "Fitur Wabot Success! Back In 10 Seconds.. [ Port Login 6401 ]"
 sleep 10
 menu
